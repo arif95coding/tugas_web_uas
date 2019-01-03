b0ae51ad1d3119e5e48e6043ce1b62873cdfaaad
nafism05 <nafism05@gmail.com>
2019-01-03 22:08:33 +0700

    first

---
+++ BootstrapDesign/keeluaran.php
@@ -0,0 +1,197 @@
+<html>
+ <head>
+ <title>
+ Hasil Output
+ </title>
+ </head>
+
+<body>
+   
+ 
+ <table width=80% align=center>
+<tr>
+<th ><img src="udinus.png" width="130" height="120"></th>
+<th><u>KARTU RENCANA STUDI MAHASISWA</font></u><br></th>
+<tr>
+<td><FONT COLOR="blue">NIM</td>
+<td><FONT COLOR="blue"><?php echo ": ".$_GET['nim']?></td> 
+<td><FONT COLOR="blue">Semester</td>
+<td width=30%><FONT COLOR="blue"> <?php echo ": ".$_GET['semester']?>
+</tr>
+
+<tr>
+<td><FONT COLOR="blue">Nama</td>
+<td><FONT COLOR="blue"><?php echo ": ".$_GET['nama']?></td>
+<td><FONT COLOR="blue">Tahun ajaran</td>
+<td><FONT COLOR="blue">: 2018-2019</td>
+</tr>    
+
+<tr>
+<td><FONT COLOR="blue">Program Studi</td>
+<td><FONT COLOR="blue">: S1-Teknik Informatika</td>
+<td><FONT COLOR="blue">Dosen Wali</td>
+<td><FONT COLOR="blue">: Nisa'ul Hafidzhoh, M.T<td>
+</tr>    
+
+<hr width=75%>
+
+<table width=80% align=center><FONT COLOR="blue">
+
+</table>
+
+<br>
+
+ <table border="1" align="center">
+
+  <tr bgcolor=#C0C0C0>
+    <th rowspan="2">No.</th>
+    <th rowspan="2">Kode MK</th>
+    <th rowspan="2">Nama Mata Kuliah</th>
+	<th rowspan="2">SKS</th>
+	<th rowspan="2">T/P</th>
+	<th rowspan="2">STS</th>
+	<th rowspan="2">Mdl</th>
+	<th rowspan="2">Klpk</th>
+	<th colspan="2">Jadwal 1</th>
+	<th colspan="2">Jadwal 2</th>
+	<th colspan="2">Jadwal 3</th>
+  </tr>
+    
+	<tr bgcolor=#C0C0C0>
+		<td>Hari, Jam</td>
+		<td>Ruang</td>
+		<td>Hari, Jam</td>
+		<td>Ruang</td>
+		<td>Hari, Jam</td>
+		<td>Ruang</td>
+	</tr>
+	
+  
+  <tr>
+	<td align="center">1.</td>
+    <td>A11.54314</td>
+    <td>PEMROGRAMAN BERBASIS WEB</td>
+	<td align="center">2</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4320</td>
+	<td>JUMAT, 14:10-16:20</td>
+	<td align="center">D2.D</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+  
+  <tr>
+	<td align="center">2.</td>
+    <td>A11.54608</td>
+    <td>PEMROGRAMAN PERANGKAT BERGERAK</td>
+	<td align="center">3</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4513</td>
+	<td>KAMIS, 15:30-18:00</td>
+	<td align="center">D2.A</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+  
+  <tr>
+	<td align="center">3.</td>
+    <td>A11.54701</td>
+    <td>BIMBINGAN KARIER</td>
+	<td align="center">2</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4701</td>
+	<td>SABTU, 10.20-12.00</td>
+	<td align="center">H3.8</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+  
+  <tr>
+	<td align="center">4.</td>
+    <td>A11.54702</td>
+    <td>KERJA PRAKTIK</td>
+	<td align="center">2</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4320</td>
+	<td>SABTU, 07.00-08.40</td>
+	<td align="center">H3.8</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+  
+  <tr>
+	<td align="center">5.</td>
+    <td>A11.54703</td>
+    <td>MANAJEMEN PROYEK</td>
+	<td align="center">3</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4504</td>
+	<td>RABU, 07.00-09.30</td>
+	<td align="center">H3.11</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+  
+  <tr>
+	<td align="center">6.</td>
+    <td>A11.54828</td>
+    <td>SOFTWARE QUALITY AND TESTING</td>
+	<td align="center">3</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4705</td>
+	<td>RABU, 09.30-12.00</td>
+	<td align="center">H5.8A</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+  
+  <tr>
+	<td align="center">7.</td>
+    <td>A11.54835</td>
+    <td>NETWORK MANAGEMENT</td>
+	<td align="center">3</td>
+    <td align="center">T</td>
+	<td align="center">B</td>
+	<td align="center">-</td>
+	<td align="center">A11.4709</td>
+	<td>KAMIS, 12.30-15.00</td>
+	<td align="center">D5.12</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+	<td align="center">-</td>
+  </tr>
+
+  <tr>
+	<td colspan="3" bgcolor=#C0C0C0 align="right">Jumlah sks</td>
+	<td bgcolor=#C0C0C0>18</td>
+  </tr>
+
+</table> 
+
+</body>
+</html>
